1edfffd22f3afef984d3eda28f39176b9563069c
Msterlinux <mrlzarate8@gmail.com>
2019-11-26 15:36:50 +0100

    checking the config
    about the post

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   chainWebpack(config){
     config.module.rule('md')
@@ -10,5 +13,22 @@ module.exports = {
       .options({
         raw: true
       })
+  },
+
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          [ '/'],
+          {
+            // options
+          }
+        ),
+      ]
+    }
   }
 }
